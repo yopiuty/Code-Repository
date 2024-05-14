14e7a3df1addecdd60a832924ebfb00187b03522
Dalgona <85731784+yopiuty@users.noreply.github.com>
2024-05-14 23:14:48 +0900

    Update helloWorld.cpp

--- C++/helloWorld.cpp
+++ C++/helloWorld.cpp
@@ -1,6 +1,8 @@
-// import <print>; 써도 되는거 확인
-import std;
+#include <iostream>
+
+// using namespace std;
 
 int main() {
-  std::println("안녕, 세상!");
+  std::cout << "안녕! 세상!" << endl;
+  return 0;
 }
